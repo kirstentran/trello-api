f0de3e5f4f756ea776fed4768b9aef79b1b0264b
Kirsten Tran <tn.phuonguyen1010@gmail.com>
2024-10-16 15:06:58 -0500

    xu ly loi backend

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -6,6 +6,7 @@
 
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
@@ -20,16 +21,18 @@ const createNew = async (req, res, next) => {
   })
 
   try {
-    console.log('REQ.Body:', req.body)
     //Set abortEarly: false để trả về tất cả các lỗi không chỉ lỗi đầu tiên
     await correctCondition.validateAsync(req.body, { abortEarly: false })
-
-    //next()
-    res.status(StatusCodes.CREATED).json({ message: 'POST from validation: API create new board' })
+//Validate dữ liệu xong xuôi hợp lệ thì cho request đi tiếp sang controller
+    next()
   } catch (error) {
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    // const errorMessages = new Error(error).message
+    // const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessages)
+    next(new ApiError (StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
+
+    // res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+    //   errors: new Error(error).message
+    // })
   }
 }
 
