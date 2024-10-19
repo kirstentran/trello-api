c8183998e099dcd2de6d4b62dbb27a861b683b62
Kirsten Tran <tn.phuonguyen1010@gmail.com>
2024-10-19 16:02:04 -0500

    write api for getboard details

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -6,6 +6,8 @@
  */
 import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async(reqBody) => {
   try {
@@ -27,6 +29,18 @@ const createNew = async(reqBody) => {
   } catch (error) { throw error}
 }
 
+const getDetails = async(boardId) => {
+  try {
+    //Goi toi tang Model de xu ly luu ban ghi newBoard vao trong database
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+    }
+    //Phai co return de tra ket qua ve trong service
+    return board
+  } catch (error) { throw error}
+}
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
