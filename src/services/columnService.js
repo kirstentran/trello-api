4c03c08e43ea8a74509818a5461285544f2e89bf
Kirsten Tran <tn.phuonguyen1010@gmail.com>
2024-10-22 23:50:51 -0500

    par1 1

---
+++ src/services/columnService.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-useless-catch */
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+import { columnModel } from '~/models/columnModel'
+
+
+const createNew = async(reqBody) => {
+  try {
+    const newColumn = {
+      ...reqBody
+    }
+    const createdColumn = await columnModel.createNew(newColumn)
+    const getNewColumn = await columnModel.findOneById(createdColumn.insertedId)
+
+    //xu ly du lieu tra ve
+    return getNewColumn
+  } catch (error) { throw error}
+}
+
+export const columnService = {
+  createNew
+}
